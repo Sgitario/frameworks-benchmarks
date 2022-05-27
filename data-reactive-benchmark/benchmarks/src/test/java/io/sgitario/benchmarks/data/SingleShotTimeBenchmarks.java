59e981ca39c3a11c59cbfa94daeb2c9bd2d1803c
Jose <josecarvajalhilario@gmail.com>
2022-05-27 06:28:00 +0200

    Delete single shot time benchmarks

--- data-reactive-benchmark/benchmarks/src/test/java/io/sgitario/benchmarks/data/SingleShotTimeBenchmarks.java
+++
@@ -1,8 +0,0 @@
-package io.sgitario.benchmarks.data;
-
-import org.openjdk.jmh.annotations.BenchmarkMode;
-import org.openjdk.jmh.annotations.Mode;
-
-@BenchmarkMode(Mode.SingleShotTime)
-public class SingleShotTimeBenchmarks extends DataBenchmarks {
-}

d7b27a436f0c0a3ab16b45a0a8e5eba07729f376
Jose <josecarvajalhilario@gmail.com>
2022-05-27 06:28:00 +0200

    Delete single shot time benchmarks

--- rest-benchmark/benchmarks/src/test/java/io/sgitario/benchmarks/rest/SingleShotTimeBenchmarks.java
+++
@@ -1,8 +0,0 @@
-package io.sgitario.benchmarks.rest;
-
-import org.openjdk.jmh.annotations.BenchmarkMode;
-import org.openjdk.jmh.annotations.Mode;
-
-@BenchmarkMode(Mode.SingleShotTime)
-public class SingleShotTimeBenchmarks extends RestBenchmarks {
-}
